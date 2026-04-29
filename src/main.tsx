701fd84267ae281f890f7c6cb5aeefdbc959579d
ChrisMS78 <chris.riekenberg@googlemail.com>
2026-04-29 16:10:11 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
 const rootElement = document.getElementById("root") as HTMLElement;
 
-createRoot(rootElement).render(<App />);
+createRoot(rootElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
